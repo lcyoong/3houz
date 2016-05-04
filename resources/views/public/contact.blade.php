d71777565eb3fc1dd05c32c22b5de152c05bc5b2
LC <lcyoong@creino.com>
2016-05-04 17:09:08 +0800

    Contact us page

--- resources/views/public/contact.blade.php
+++ resources/views/public/contact.blade.php
@@ -17,12 +17,30 @@
       </div>
   </header>
   <div class="container">
+    <div class="page-header">
+      <h1>Get in touch!</h1>
+    </div>
     <div class="row">
       <div class="col-md-6">
-        <i class="fa fa-envelope"></i> <a href="mailto:{{ config('3houz.email') }}">{{ config('3houz.email') }}</a>
+        <h3><i class="fa fa-envelope"></i> <a href="mailto:{{ config('3houz.email') }}">{{ config('3houz.email') }}</a></h3>
       </div>
       <div class="col-md-6">
+        <h3><i class="fa fa-phone"></i> {{ config('3houz.mobile') }}</h3>
       </div>
     </div>
+    <a href="#" class="popupchat">*Or leave a message on our chat widget</a>
   </div>
+@endsection
+
+@section('js')
+<script>
+$(document).ready(function () {
+  $('body').on('click', '.popupchat', function (event) {
+    event.preventDefault();
+    $zopim(function() {
+      $zopim.livechat.window.show();
+    });
+  });
+});
+</script>
 @endsection
