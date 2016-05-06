c2d8d10881b08d8c7f8fb6bc4b35ffc9c2382369
LC <lcyoong@creino.com>
2016-05-06 11:10:52 +0800

    Make property address required

--- app/Http/Requests/CreateProperty.php
+++ app/Http/Requests/CreateProperty.php
@@ -32,6 +32,7 @@ class CreateProperty extends Request
             'prop_furnishing' => 'required',
             'prop_key' => 'required|min:4',
             'prop_location' => 'required',
+            'prop_address' => 'required',
             'prop_no_bedrooms' => 'required|integer|min:1',
             'prop_no_bathrooms' => 'required|integer|min:1',
             'prop_built_up' => 'required|integer|min:1',
